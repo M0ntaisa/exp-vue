fb0a7f2247b1e0cf78e85feefdc35d73b333cc5e
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-03-16 14:00:57 +0800

    add product

---
+++ frontend/src/components/AddProduct.vue
@@ -0,0 +1,57 @@
+<template lang="">
+  <div>
+    <div class="field">
+      <label class="label">Product Name</label>
+    </div>
+    <div class="control">
+      <input type="text" class="input" placeholder="Product Name" v-model="productName" />
+    </div>
+  </div>
+  <div>
+    <div class="field">
+      <label class="label">Product Price</label>
+    </div>
+    <div class="control">
+      <input type="text" class="input" placeholder="Price" v-model="productPrice" />
+    </div>
+  </div>
+  <div class="control">
+    <button class="button is-success mt-5" @click="saveProduct">
+      SAVE
+    </button>
+  </div>
+</template>
+
+<script>
+// import axios
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      productName: "",
+      productPrce: "",
+    };
+  },
+  methods: {
+    // create new product
+    async saveProduct() {
+      try {
+        await axios.post("http://localhost:5000/products", {
+          product_name:this.productName,
+          product_price:this.productPrice,
+        });
+        (this.productName = ""),
+        (this.productPrice = "");
+        this.$router.push("/index");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="">
+  
+</style>
