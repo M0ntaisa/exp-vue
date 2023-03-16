989ea52f118be59258e693ef0c79b5246959689c
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-03-16 10:24:59 +0800

    add router

---
+++ backend/router/routes.js
@@ -0,0 +1,32 @@
+// import express
+import express from "express";
+
+// import functions from controllers
+import { 
+  showProducts,
+  showSingleProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/product.js";
+
+// init express router
+const router = express.Router();
+
+// all products
+router.get("/products", showProducts);
+
+// single product
+router.get("/products/:id", showSingleProduct);
+
+// create new product
+router.post("/products", createProduct);
+
+// update product
+router.put("/products/:id", updateProduct);
+
+// delete product
+router.delete("products/:id", deleteProduct);
+
+// export default router
+export default router;
